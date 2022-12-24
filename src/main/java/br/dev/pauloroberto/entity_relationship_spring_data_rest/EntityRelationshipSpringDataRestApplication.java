927777519e87cbf42e86b0abae77ace0f8179bde
Paulo Roberto Vieira Ribeiro Cavalcanti <paulor.vrcavalcanti@gmail.com>
2022-12-24 08:52:55 -0400

    First commit

---
+++ src/main/java/br/dev/pauloroberto/entity_relationship_spring_data_rest/EntityRelationshipSpringDataRestApplication.java
@@ -0,0 +1,13 @@
+package br.dev.pauloroberto.entity_relationship_spring_data_rest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EntityRelationshipSpringDataRestApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EntityRelationshipSpringDataRestApplication.class, args);
+    }
+
+}
---
+++ src/test/java/br/dev/pauloroberto/entity_relationship_spring_data_rest/EntityRelationshipSpringDataRestApplicationTests.java
@@ -0,0 +1,13 @@
+package br.dev.pauloroberto.entity_relationship_spring_data_rest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EntityRelationshipSpringDataRestApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
