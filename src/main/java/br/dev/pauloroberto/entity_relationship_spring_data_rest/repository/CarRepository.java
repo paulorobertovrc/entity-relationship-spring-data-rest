1cb5811ed7eafdd2ae81c4be1b0675d7b058797a
Paulo Roberto Vieira Ribeiro Cavalcanti <paulor.vrcavalcanti@gmail.com>
2022-12-26 20:44:33 -0400

    Implemented findByLicenseNumber method

--- src/main/java/br/dev/pauloroberto/entity_relationship_spring_data_rest/repository/CarRepository.java
+++ src/main/java/br/dev/pauloroberto/entity_relationship_spring_data_rest/repository/CarRepository.java
@@ -4,4 +4,5 @@ import br.dev.pauloroberto.entity_relationship_spring_data_rest.model.Car;
 import org.springframework.data.repository.CrudRepository;
 
 public interface CarRepository extends CrudRepository<Car, Long> {
+    Car findByLicenseNumber(String licenseNumber);
 }
