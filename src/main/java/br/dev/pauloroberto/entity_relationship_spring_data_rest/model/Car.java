81a84fa382c7deb9ab43dc2fdb6c25497c0d1db5
Paulo Roberto Vieira Ribeiro Cavalcanti <paulor.vrcavalcanti@gmail.com>
2022-12-26 21:42:55 -0400

    Added @Getter annotation and column licensePlate now is unique.

--- src/main/java/br/dev/pauloroberto/entity_relationship_spring_data_rest/model/Car.java
+++ src/main/java/br/dev/pauloroberto/entity_relationship_spring_data_rest/model/Car.java
@@ -1,28 +1,25 @@
 package br.dev.pauloroberto.entity_relationship_spring_data_rest.model;
 
 import br.dev.pauloroberto.entity_relationship_spring_data_rest.dto.CarDto;
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
-import lombok.AllArgsConstructor;
-import lombok.EqualsAndHashCode;
-import lombok.NoArgsConstructor;
-import lombok.ToString;
+import jakarta.persistence.*;
+import lombok.*;
 
 @Entity
 @AllArgsConstructor
 @NoArgsConstructor
+@Getter
 @EqualsAndHashCode(of = "id")
 @ToString
 public class Car {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+    @Column(nullable = false, unique = true)
     private String licenseNumber;
     private Brand brand;
     private String model;
     private String color;
+    @Column(nullable = false)
     private boolean available;
 
     public Car(CarDto carDto) {
