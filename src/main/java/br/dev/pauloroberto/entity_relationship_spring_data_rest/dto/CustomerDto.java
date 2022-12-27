e57df2e8cb2ae5413b0a9b0e5ec631eaaeed28f3
Paulo Roberto Vieira Ribeiro Cavalcanti <paulor.vrcavalcanti@gmail.com>
2022-12-27 14:02:17 -0400

    Now it's not required to inform an address when instantiating a Customer.

--- src/main/java/br/dev/pauloroberto/entity_relationship_spring_data_rest/dto/CustomerDto.java
+++ src/main/java/br/dev/pauloroberto/entity_relationship_spring_data_rest/dto/CustomerDto.java
@@ -4,16 +4,16 @@ import br.dev.pauloroberto.entity_relationship_spring_data_rest.model.Rental;
 import jakarta.validation.Valid;
 import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import java.util.List;
 
 public record CustomerDto(
         @NotBlank // Checks if the field (string) is not null and not empty
         String name,
-        @NotBlank
-        @Email // Checks if the field is a valid email
+        @NotBlank @Email // Checks if the field is a valid email
         String email,
-        @Valid
+        @RequestParam(required = false) @Valid
         AddressDto address,
         List<Rental> rentals
 ) {
