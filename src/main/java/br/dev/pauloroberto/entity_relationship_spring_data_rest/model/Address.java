3f75c450ed88d93552c522fa2692225629858fc8
Paulo Roberto Vieira Ribeiro Cavalcanti <paulor.vrcavalcanti@gmail.com>
2022-12-27 13:57:55 -0400

    Removed unused methods

--- src/main/java/br/dev/pauloroberto/entity_relationship_spring_data_rest/model/Address.java
+++ src/main/java/br/dev/pauloroberto/entity_relationship_spring_data_rest/model/Address.java
@@ -38,84 +38,12 @@ public class Address {
     private Customer customer;
 
     public Address(AddressDto address) {
-        this.logradouro = address.logradouro();
-        this.numero = address.numero();
-        this.complemento = address.complemento();
-        this.bairro = address.bairro();
-        this.cep = address.cep();
-        this.cidade = address.cidade();
-        this.uf = address.uf();
-    }
-
-    public Long getId() {
-        return id;
-    }
-
-    public void setId(Long id) {
-        this.id = id;
-    }
-
-    public String getLogradouro() {
-        return logradouro;
-    }
-
-    public void setLogradouro(String logradouro) {
-        this.logradouro = logradouro;
-    }
-
-    public String getNumero() {
-        return numero;
-    }
-
-    public void setNumero(String numero) {
-        this.numero = numero;
-    }
-
-    public String getComplemento() {
-        return complemento;
-    }
-
-    public void setComplemento(String complemento) {
-        this.complemento = complemento;
-    }
-
-    public String getBairro() {
-        return bairro;
-    }
-
-    public void setBairro(String bairro) {
-        this.bairro = bairro;
-    }
-
-    public String getCep() {
-        return cep;
-    }
-
-    public void setCep(String cep) {
-        this.cep = cep;
-    }
-
-    public String getCidade() {
-        return cidade;
-    }
-
-    public void setCidade(String cidade) {
-        this.cidade = cidade;
-    }
-
-    public String getUf() {
-        return uf;
-    }
-
-    public void setUf(String uf) {
-        this.uf = uf;
-    }
-
-    public Customer getCustomer() {
-        return customer;
-    }
-
-    public void setCustomer(Customer customer) {
-        this.customer = customer;
+            this.logradouro = address.logradouro();
+            this.numero = address.numero();
+            this.complemento = address.complemento();
+            this.bairro = address.bairro();
+            this.cep = address.cep();
+            this.cidade = address.cidade();
+            this.uf = address.uf();
     }
 }
