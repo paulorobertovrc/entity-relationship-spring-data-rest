ba3b265502ff32caa6b11c7a5471c9c902a70841
Paulo Roberto Vieira Ribeiro Cavalcanti <paulor.vrcavalcanti@gmail.com>
2022-12-26 20:43:40 -0400

    Car attribute is no longer a List and removed unused methods

--- src/main/java/br/dev/pauloroberto/entity_relationship_spring_data_rest/model/Car.java
+++ src/main/java/br/dev/pauloroberto/entity_relationship_spring_data_rest/model/Car.java
@@ -1,13 +1,14 @@
 package br.dev.pauloroberto.entity_relationship_spring_data_rest.model;
 
 import br.dev.pauloroberto.entity_relationship_spring_data_rest.dto.CarDto;
-import jakarta.persistence.*;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
 import lombok.AllArgsConstructor;
 import lombok.EqualsAndHashCode;
 import lombok.NoArgsConstructor;
 import lombok.ToString;
-
-import java.util.List;
 
 @Entity
 @AllArgsConstructor
@@ -24,51 +25,12 @@ public class Car {
     private String color;
     private boolean available;
 
-    @ManyToMany(mappedBy = "cars", fetch = FetchType.EAGER)
-    private List<Rental> rentals;
-
     public Car(CarDto carDto) {
         this.licenseNumber = carDto.licenseNumber();
         this.brand = carDto.brand();
         this.model = carDto.model();
         this.color = carDto.color();
         this.available = carDto.available();
-    }
-
-    public Long getId() {
-        return id;
-    }
-
-    public String getLicenseNumber() {
-        return licenseNumber;
-    }
-
-    public void setLicenseNumber(String licenseNumber) {
-        this.licenseNumber = licenseNumber;
-    }
-
-    public Brand getBrand() {
-        return brand;
-    }
-
-    public void setBrand(Brand brand) {
-        this.brand = brand;
-    }
-
-    public String getModel() {
-        return model;
-    }
-
-    public void setModel(String model) {
-        this.model = model;
-    }
-
-    public String getColor() {
-        return color;
-    }
-
-    public void setColor(String color) {
-        this.color = color;
     }
 
     public boolean isAvailable() {
