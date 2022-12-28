bde73eca9618a4fd2c9d7fd2d7bacba13698d864
Paulo Roberto Vieira Ribeiro Cavalcanti <paulor.vrcavalcanti@gmail.com>
2022-12-28 12:44:56 -0400

    AddressController

--- src/main/java/br/dev/pauloroberto/entity_relationship_spring_data_rest/controller/AddressController.java
+++ src/main/java/br/dev/pauloroberto/entity_relationship_spring_data_rest/controller/AddressController.java
@@ -5,10 +5,7 @@ import br.dev.pauloroberto.entity_relationship_spring_data_rest.model.Address;
 import br.dev.pauloroberto.entity_relationship_spring_data_rest.repository.AddressRepository;
 import jakarta.validation.Valid;
 import org.springframework.transaction.annotation.Transactional;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/addresses")
