d4be510a42c67fb6f74c1abdff4c5a5a2c0d7f34
Paulo Roberto Vieira Ribeiro Cavalcanti <paulor.vrcavalcanti@gmail.com>
2022-12-30 18:50:05 -0400

    This DTO now checks whether the fields sent by the client are blank or null and, if so, fetches in an external API and fill them. Also added some annotations.

--- src/main/java/br/dev/pauloroberto/entity_relationship_spring_data_rest/dto/AddressDto.java
+++ src/main/java/br/dev/pauloroberto/entity_relationship_spring_data_rest/dto/AddressDto.java
@@ -1,16 +1,28 @@
 package br.dev.pauloroberto.entity_relationship_spring_data_rest.dto;
 
+import br.dev.pauloroberto.entity_relationship_spring_data_rest.service.FetchAddressApiService;
+import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.Pattern;
+import org.springframework.web.bind.annotation.RequestParam;
 
 public record AddressDto(
-        String logradouro,
-        String numero,
-        String complemento,
-        String bairro,
+        @RequestParam(required = false) String logradouro,
+        @NotBlank(message = "The field NUMBER must not be blank") String numero,
+        @RequestParam(required = false) String complemento,
+        @RequestParam(required = false) String bairro,
         @Pattern(regexp = "\\d{5}-\\d{3}") // Checks if the field is a valid zip code
         String cep,
-        String cidade,
-        @Pattern(regexp = "[A-Z]{2}") // Checks if the field is a valid state
+        @RequestParam(required = false) String cidade,
+        @RequestParam(required = false) @Pattern(regexp = "(^$)|([A-Z]{2})") // Checks if the field is a valid state
         String uf
 ) {
+    public AddressDto {
+        if (logradouro == null || logradouro.isBlank()) {
+            FetchedAddressFromApiDto addressFetchApiDto = new FetchAddressApiService().getCityByZipCode(cep);
+            logradouro = addressFetchApiDto.address_type() + " " + addressFetchApiDto.address_name();
+            bairro = addressFetchApiDto.district();
+            cidade = addressFetchApiDto.city();
+            uf = addressFetchApiDto.state();
+        }
+    }
 }
